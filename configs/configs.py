14c7c428dbfa1cf3017de50a690ea5e48f51ef3f
DevBruce <bruce93k@gmail.com>
2021-03-27 19:36:55 +0900

    Modify lr schedule

--- configs/configs.py
+++ configs/configs.py
@@ -22,7 +22,7 @@ class Configs:
         self.epochs = 105          # Paper Default: 135 (75 (lr: 1e-2) + 30 (lr: 1e-3) + 30 (lr: 1e-4))
         self.init_lr = 1e-4        # Paper Default: 1e-2
         self.lr_decay_rate = 0.5
-        self.lr_decay_steps = 10000
+        self.lr_decay_steps = 4000
         self.batch_size = 32
         self.val_step = 1
         self.tb_img_max_outputs = 6
