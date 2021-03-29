dca0601ecaaf7ba728bafa75e44b4e19aa6aa578
DevBruce <bruce93k@gmail.com>
2021-03-29 19:55:56 +0900

    Update voc_tfds

--- datasets/voc_tfds/voc.py
+++ datasets/voc_tfds/voc.py
@@ -13,29 +13,29 @@ class GetVoc:
         self.autotune = tf.data.experimental.AUTOTUNE
 
     def get_train_ds(self, shuffle=True, drop_remainder=True):
-        train_ds, ds_info = tfds.load(name='voc/2012', split=['train'], with_info=True)
-        train_ds = train_ds[0]
+        # Training Dataset (voc2007 trainval + 2012 trainval)
+        (voc2007_train, voc2007_val), voc2007_info = tfds.load(name='voc/2007', split=['train', 'validation'], with_info=True)
+        (voc2012_train, voc2012_val), voc2012_info = tfds.load(name='voc/2012', split=['train', 'validation'], with_info=True)
+        train_ds = voc2007_train.concatenate(voc2007_val).concatenate(voc2012_train).concatenate(voc2012_val)
+        train_ds_num_examples = voc2007_info.splits['train'].num_examples + \
+            voc2007_info.splits['validation'].num_examples + \
+            voc2012_info.splits['train'].num_examples + \
+            voc2012_info.splits['validation'].num_examples
+        
         train_ds = train_ds.map(normalize_img, num_parallel_calls=self.autotune)
         train_ds = train_ds.map(flip_lr, num_parallel_calls=self.autotune)
         train_ds = train_ds.map(color_augs, num_parallel_calls=self.autotune)
         train_ds = train_ds.cache()  # Loaded data first time, it's going to keep track of some of them in memory. It makes faster
         if shuffle:
-            train_ds = train_ds.shuffle(ds_info.splits['train'].num_examples)
+            train_ds = train_ds.shuffle(train_ds_num_examples)
         train_ds = train_ds.padded_batch(self.batch_size, drop_remainder=drop_remainder)
         train_ds = train_ds.prefetch(self.autotune)  # While running on gpu, it's going to prefetch number of batch_size examples, so they are ready to be run instantly after the gpu calls are done
         return train_ds
 
     def get_val_ds(self):
-        val_ds = tfds.load(name='voc/2012', split=['validation'], with_info=False)[0]
+        (val_ds,) = tfds.load(name='voc/2007', split=['test'], with_info=False)
         val_ds = val_ds.map(normalize_img, num_parallel_calls=self.autotune)
         val_ds = val_ds.padded_batch(self.batch_size)
         val_ds = val_ds.prefetch(self.autotune)
         return val_ds
-
-    def get_test_ds(self):
-        test_ds = tfds.load(name='voc/2012', split=['test'], with_info=False)[0]
-        test_ds = test_ds.map(normalize_img, num_parallel_calls=self.autotune)
-        test_ds = test_ds.padded_batch(self.batch_size)
-        test_ds = test_ds.prefetch(self.autotune)
-        return test_ds
     
