971678b40371dac5161c11c818c1b9001800befe
DevBruce <bruce93k@gmail.com>
2021-04-07 13:23:41 +0900

    Update augments

--- datasets/voc_tfds/augs.py
+++ datasets/voc_tfds/augs.py
@@ -18,16 +18,15 @@ def get_transform(img_height, img_width, input_height, input_width, val=False):
             ),
         )
     else:  # Training --> Augment data
-        h_crop_ratio = np.random.uniform(low=0.25, high=1.)
-        w_crop_ratio = np.random.uniform(low=0.25, high=1.)
+        h_crop_ratio = np.random.uniform(low=0.25, high=0.9)
+        w_crop_ratio = np.random.uniform(low=0.25, high=0.9)
         h_crop = int(img_height * h_crop_ratio)
         w_crop = int(img_width * w_crop_ratio)
         transform = A.Compose(
             [
                 A.HorizontalFlip(p=0.5),
                 A.RandomCrop(width=w_crop, height=h_crop, p=0.5),
-                A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
-                A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+                A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, p=0.5),
                 A.Resize(input_height, input_width, p=1.),
             ],
             bbox_params=A.BboxParams(
